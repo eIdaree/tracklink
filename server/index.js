d2c1443f4c5d1bf6b07d5fc94ba517f1993fe3fa
eIdaree <159745986+eIdaree@users.noreply.github.com>
2024-09-25 10:22:58 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,30 +5,19 @@ const app = express();
 app.get('/track', async (req, res) => {
   const track_url = decodeURIComponent(req.query.track_url);
   const redirect_url = decodeURIComponent(req.query.redirect_url);  
-
-  console.log('track_url:', track_url);
-  console.log('redirect_url:', redirect_url);
-  
   if (!track_url || !redirect_url) {
     return res.status(400).send('Missing track_url or redirect_url parameters');
   }
-
   try {
     const urlObj = new URL(track_url);
     const baseUrl = urlObj.origin + urlObj.pathname;
     const params = new URLSearchParams(urlObj.search);
-
-    console.log('Base URL:', baseUrl);
-    console.log('Params:', params.toString());
-
     // Perform the request to track_url
     const response = await axios.get(baseUrl, { params: Object.fromEntries(params.entries()) });
-
     // Check the response status
     if (response.status < 200 || response.status >= 300) {
       throw new Error(`Request failed with status ${response.status}`);
     }
-
     // Redirect to the original URL
     res.redirect(redirect_url);
   } catch (error) {
